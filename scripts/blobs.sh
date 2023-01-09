10724a94c0c55cb34bced0284fc5b0335c552bfe
Mahmoud <mahmoud.jahanshahi@gmail.com>
2023-01-09 16:42:19 -0500

    copiedb2tP

--- scripts/blobs.sh
+++ scripts/blobs.sh
@@ -6,17 +6,17 @@ ver=U;
 dir="/nfs/home/audris/work/c2fb/";
 for i in {10,50,109}; do
     echo "Test file $i";
-    n=$(zcat ${dir}b2tPFull${ver}$i.s | 
+    n1=$(zcat ${dir}b2tPFull${ver}$i.s | 
         cut -d\; -f1 | 
         uniq | 
         wc -l);
-    echo "b2tP: $n";
+    echo "b2tP: $n1";
     nc=$(zcat data/b2tPFull${ver}$i.copied | 
         wc -l);
     nnc=$(zcat data/notCopiedb2PFull${ver}$i.s | 
         wc -l);
-    n=$((nc + nnc));
-    echo "b2c-nc: $n";
+    n2=$((nc + nnc));
+    echo "b2c-nc: $n2";
     echo ;
 done;
 
@@ -25,9 +25,18 @@ echo "total copied blobs:";
 for i in {0..127}; do
     zcat data/b2tPFull${ver}$i.copied;
 done |
-wc -l;
+wc -l; #1,084,211,945
 echo "total not-copied blobs:";
 for i in {0..127}; do
     zcat data/notCopiedb2PFull${ver}$i.s;
 done |
-wc -l;
+wc -l; #
+
+# copiedb2tP
+for i in {0..127}; do 
+    LC_ALL=C LANG=C join -t\; \
+        <(zcat ${dir}b2tPFull${ver}$i.s) \
+        <(zcat data/b2tPFull${ver}$i.copied) |
+    gzip >data/copiedb2tPFull${ver}$i.s;
+    echo "File $i finished!"
+done;
